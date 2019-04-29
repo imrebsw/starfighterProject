97ee60dec614d83efcc35b10b9ef45e36479d323
rebekah <33032837+imrebsw@users.noreply.github.com>
2019-04-29 07:32:30 -0700

    Update OuterSpace.java

--- src/starfighter/OuterSpace.java
+++ src/starfighter/OuterSpace.java
@@ -7,38 +7,66 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Canvas;
+import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import static java.lang.Character.*;
 import java.awt.image.BufferedImage;
 import java.awt.event.ActionListener;
+import java.net.URL;
 import java.util.ArrayList;
+import javax.imageio.ImageIO;
 
 public class OuterSpace extends Canvas implements KeyListener, Runnable {
 
     private Ship ship;
-    //private Alien alienOne;
-    //private Alien alienTwo;
-    private Bullets bullet;
-    // uncomment once you are ready for this part
+    private Alien alienOne;
+    private Alien alienTwo;
     private AlienHorde horde;
-    private Bullets shots;
-   
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private int resets = 0;
+    private long score = 0;
+    private int timesince = 0;
+    private int lives = 3;
+    private Image shipPic;
+    private int timeSinceDeath = 0;
+    private long nextGoal =20000;
+    /* uncomment once you are ready for this part
+     *
+     private AlienHorde horde;
+     private Bullets shots;
+     */
     private boolean[] keys;
     private BufferedImage back;
+    private Bullets bullets;
 
     public OuterSpace() {
+        try {
+            URL url = getClass().getResource("/images/ship.jpg");
+            shipPic = ImageIO.read(url);
+        } catch (Exception e) {
+            //feel free to do something here
+            System.out.println("You messed up the ship class");
+        }
         setBackground(Color.black);
 
         keys = new boolean[5];
 
         //instantiate other instance variables
         //Ship, Alien
-        ship = new Ship(300,300,60,60,2);
-        horde = new AlienHorde(10);
-        bullet = new Bullets();
-        shots = new Bullets();
+        ship = new Ship(70, 70, 60, 50, 3);
+        alienOne = new Alien(70, 70, 60, 50, 3);
+        horde = new AlienHorde(0);
+        bullets = new Bullets();
+        //horde.add(alienOne);
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 6; x++) {
+                horde.add(new Alien(70 + 100 * x, 70 + 100 * y, 60, 50, 3));
+            }
+        }
         this.addKeyListener(this);
         new Thread(this).start();
 
@@ -62,12 +90,45 @@ public class OuterSpace extends Canvas implements KeyListener, Runnable {
         //create a graphics reference to the back ground image
         //we will draw all changes on the background image
         Graphics graphToBack = back.createGraphics();
-        graphToBack.setColor(Color.BLUE);
-        graphToBack.drawString("StarFighter ", 25, 50);
+
+        if (horde.isEmpty()) {
+            resets++;
+            for (int y = 0; y < 3; y++) {
+                for (int x = 0; x < 6; x++) {
+                    horde.add(new Alien(70 + 100 * x, 70 + 100 * y, 60, 50, 3));
+                }
+            }
+        }
+        
+        if (score>nextGoal){
+            nextGoal+=10000;
+            lives++;
+        }
+        if(lives<0){
+            lives=3;
+            horde.clear();
+            score=0;
+        }
         graphToBack.setColor(Color.BLACK);
         graphToBack.fillRect(0, 0, 800, 600);
-	
-        //add code to move Ship, Alien, etc.
+        timesince++;
+        timeSinceDeath++;
+        
+        if((int) (Math.random() * 300)==(int) (Math.random() * 300))
+            horde.activate((int)(Math.random() * (horde.getList().size()-1)));
+        
+        graphToBack.setColor(Color.BLUE);
+        graphToBack.drawString("StarFighter ", 25, 50);
+        bullets.drawAll(graphToBack);
+        bullets.moveAll();
+        for(int i=0;i<lives;i++)
+            graphToBack.drawImage(shipPic, 100+30*i, 100, 30, 30, null);
+        graphToBack.setColor(Color.white);
+        
+        Font font = new Font("Helvetica", Font.PLAIN, 24);
+        graphToBack.setFont(font);
+        graphToBack.drawString("Score: " + score, 100, 100);
+
         if (keys[0] == true) {
             ship.move("LEFT");
         }
@@ -81,49 +142,76 @@ public class OuterSpace extends Canvas implements KeyListener, Runnable {
             ship.move("DOWN");
         }
         if (keys[4] == true) {
-            bullet.add(new Ammo(ship.getX()+ship.getWidth()/2-3,ship.getY()));
-            keys[4] = false;
+            if (timesince > 50&&timeSinceDeath>250) {
+                timesince = 0;
+                bullets.add(new Ammo(ship.getX(), ship.getY(), 3));
+                bullets.add(new Ammo(ship.getX() + ship.getWidth() - 10, ship.getY(), 3));
+            }
         }
-        ship.draw(graphToBack);
-        horde.drawEmAll(graphToBack);
-        horde.moveEmAll();
-        bullet.drawEmAll(graphToBack);
-        bullet.moveEmAll();
-
         
+        if(timeSinceDeath>250){
+            horde.moveAll();
+            ship.draw(graphToBack);
+            checkShip();}
+        else 
+            if(timeSinceDeath%(101-timeSinceDeath/5)<10)
+                ship.draw(graphToBack);
 
-        
-        //add in collision detection to see if Bullets hit the Aliens and if Bullets hit the Ship
+        check();
+
+
+        horde.drawAll(graphToBack);
+        //horde.moveAll();
+
+        //alienOne.draw(graphToBack);
+        //add code to move Shidap, Alien, etc.
         if (ship.getX() < 0) {
+            ship.setX(WIDTH);
+        }
+        if (ship.getX() > WIDTH) {
             ship.setX(0);
         }
-        if (ship.getX() > 800 - ship.getWidth()) {
-            ship.setX(800 - ship.getWidth());
-        }
         if (ship.getY() < 0) {
+            ship.setY(HEIGHT);
+        }
+        if (ship.getY() > HEIGHT) {
             ship.setY(0);
         }
-        if (ship.getY() > 600 - ship.getHeight()) {
-            ship.setY(600 - ship.getHeight());
-        }
-        
-        /*for (Ammo a : bullet.getList()) {
-            if (a.getY() >= alienOne.getY() &&a.getY()<=alienOne.getY()+alienOne.getHeight()&&a.getX() >= alienOne.getX()&&a.getX()<= alienOne.getX()+alienOne.getWidth() ) {
-                alienOne.setHeight(0);
-                alienOne.setWidth(0);
-                a.setWidth(0);
-                a.setHeight(0);
-            }
-            if (a.getY() >= alienTwo.getY() && a.getY()<=alienTwo.getY()+alienTwo.getHeight()&& a.getX() >= alienTwo.getX()&&a.getX()<= alienTwo.getX()+alienTwo.getWidth()) {
-                alienTwo.setHeight(0);
-                alienTwo.setWidth(0);
-                a.setWidth(0);
-                a.setHeight(0);
-            }
-        }
-        */
-        horde.removeDeadOnes(bullet.getList());
+        //add in collision detection to see if Bullets hit the Aliens and if Bullets hit the Ship
         twoDGraph.drawImage(back, null, 0, 0);
+    }
+
+    public void checkShip() {
+            for (int j = 0; j < horde.getList().size(); j++) {
+                Alien tempa = horde.getList().get(j);
+                if (ship.getX() > tempa.getX() && ship.getX() < tempa.getX() + tempa.getWidth()) {
+                    if (ship.getY() + ship.getHeight() > tempa.getY() && ship.getY() < tempa.getY() + tempa.getHeight()) {
+                        horde.remove(j);
+                        timeSinceDeath=0;
+                        lives--;
+
+                        return;
+                    }
+                }
+            }
+
+    }
+
+    public void check() {
+        for (int i = 0; i < bullets.getList().size(); i++) {
+            for (int j = 0; j < horde.getList().size(); j++) {
+                Alien tempa = horde.getList().get(j);
+                Ammo tempb = bullets.getList().get(i);
+                if (tempb.getX() > tempa.getX() && tempb.getX() < tempa.getX() + tempa.getWidth()) {
+                    if (tempb.getY() + tempb.getHeight() > tempa.getY() && tempb.getY() < tempa.getY() + tempa.getHeight()) {
+                        horde.remove(j);
+                        bullets.remove(i);
+                        score += 100;
+                        return;
+                    }
+                }
+            }
+        }
     }
 
     public void keyPressed(KeyEvent e) {
