b361dcfb08d3d8a4dfa0586f28e164324e93656d
rebekah <33032837+imrebsw@users.noreply.github.com>
2019-04-29 07:32:57 -0700

    Update Ship.java

--- src/starfighter/Ship.java
+++ src/starfighter/Ship.java
@@ -8,6 +8,7 @@ import java.net.URL;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import static java.lang.Character.toUpperCase;
 import javax.imageio.ImageIO;
 
 public class Ship extends MovingThing {
@@ -20,29 +21,32 @@ public class Ship extends MovingThing {
     }
 
     public Ship(int x, int y) {
-        super(x, y);
+        this(x, y, 10, 10, 10);
+        //add code here
+
     }
 
     public Ship(int x, int y, int s) {
-        //add code here
-        super(x, y);
-        speed = s;
-
+        this(x, y, 10, 10, s);
     }
 
     public Ship(int x, int y, int w, int h, int s) {
         super(x, y, w, h);
         speed = s;
+        loadImage();
+    }
+
+    private void loadImage() {
         try {
             URL url = getClass().getResource("/images/ship.jpg");
             image = ImageIO.read(url);
         } catch (Exception e) {
             //feel free to do something here
+            System.out.println("You messed up the ship class");
         }
     }
 
     public void setSpeed(int s) {
-        //add more code
         speed = s;
     }
 
@@ -51,25 +55,30 @@ public class Ship extends MovingThing {
     }
 
     public void move(String direction) {
-        if (direction.equals("DOWN")) {
-            super.setY(super.getY() + getSpeed());
-        }
-        if (direction.equals("UP")) {
-            super.setY(super.getY() - getSpeed());
-        }
-        if (direction.equals("LEFT")) {
-            super.setX(super.getX() - getSpeed());
-        }
-        if (direction.equals("RIGHT")) {
-            super.setX(super.getX() + getSpeed());
+        switch (direction) {
+            case "UP":
+                setY(getY() - speed);
+                break;
+            case "DOWN":
+                setY(getY() + speed);
+                break;
+            case "RIGHT":
+                setX(getX() + speed);
+                break;
+            case "LEFT":
+                setX(getX() - speed);
+                break;
         }
     }
 
     public void draw(Graphics window) {
         window.drawImage(image, getX(), getY(), getWidth(), getHeight(), null);
+
+        //window.setColor(Color.white);
+        //window.drawRect(getX(), getY(), getWidth(), getHeight());
     }
 
     public String toString() {
-        return super.toString() + getSpeed();
+        return super.toString() + " " + getSpeed();
     }
 }
