fff0ae4adeedbc8bafedef8c5cd46914a0def867
rebekah <33032837+imrebsw@users.noreply.github.com>
2019-04-28 23:01:19 -0700

    Update OuterSpace.java

--- src/starfighter/OuterSpace.java
+++ src/starfighter/OuterSpace.java
@@ -3,8 +3,6 @@ package starfighter;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
-
-import starfighter.Alien;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -17,154 +15,166 @@ import java.awt.image.BufferedImage;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class OuterSpace extends Canvas implements KeyListener, Runnable
-{
-	private Ship ship;
-	private Alien alienOne;
-	private Alien alienTwo;
+public class OuterSpace extends Canvas implements KeyListener, Runnable {
 
-	/* uncomment once you are ready for this part
-	 *
-   private AlienHorde horde;
-	private Bullets shots;
-	*/
+    private Ship ship;
+    //private Alien alienOne;
+    //private Alien alienTwo;
+    private Bullets bullet;
+    // uncomment once you are ready for this part
+    private AlienHorde horde;
+    private Bullets shots;
+   
+    private boolean[] keys;
+    private BufferedImage back;
 
-	private boolean[] keys;
-	private BufferedImage back;
+    public OuterSpace() {
+        setBackground(Color.black);
 
-	public OuterSpace()
-	{
-		setBackground(Color.black);
+        keys = new boolean[5];
 
-		keys = new boolean[5];
+        //instantiate other instance variables
+        //Ship, Alien
+        ship = new Ship(300,300,60,60,2);
+        horde = new AlienHorde(10);
+        bullet = new Bullets();
+        shots = new Bullets();
+        this.addKeyListener(this);
+        new Thread(this).start();
 
-		//instantiate other instance variables
-                Ship ship = new Ship();
-		//Ship, Alien
+        setVisible(true);
+    }
 
-		this.addKeyListener(this);
-		new Thread(this).start();
+    public void update(Graphics window) {
+        paint(window);
+    }
 
-		setVisible(true);
-	}
+    public void paint(Graphics window) {
+        //set up the double buffering to make the game animation nice and smooth
+        Graphics2D twoDGraph = (Graphics2D) window;
 
-   public void update(Graphics window)
-   {
-	   paint(window);
-   }
+        //take a snap shop of the current screen and same it as an image
+        //that is the exact same width and height as the current screen
+        if (back == null) {
+            back = (BufferedImage) (createImage(getWidth(), getHeight()));
+        }
 
-	public void paint( Graphics window )
-	{
-		//set up the double buffering to make the game animation nice and smooth
-		Graphics2D twoDGraph = (Graphics2D)window;
+        //create a graphics reference to the back ground image
+        //we will draw all changes on the background image
+        Graphics graphToBack = back.createGraphics();
+        graphToBack.setColor(Color.BLUE);
+        graphToBack.drawString("StarFighter ", 25, 50);
+        graphToBack.setColor(Color.BLACK);
+        graphToBack.fillRect(0, 0, 800, 600);
+	
+        //add code to move Ship, Alien, etc.
+        if (keys[0] == true) {
+            ship.move("LEFT");
+        }
+        if (keys[1] == true) {
+            ship.move("RIGHT");
+        }
+        if (keys[2] == true) {
+            ship.move("UP");
+        }
+        if (keys[3] == true) {
+            ship.move("DOWN");
+        }
+        if (keys[4] == true) {
+            bullet.add(new Ammo(ship.getX()+ship.getWidth()/2-3,ship.getY()));
+            keys[4] = false;
+        }
+        ship.draw(graphToBack);
+        horde.drawEmAll(graphToBack);
+        horde.moveEmAll();
+        bullet.drawEmAll(graphToBack);
+        bullet.moveEmAll();
 
-		//take a snap shop of the current screen and same it as an image
-		//that is the exact same width and height as the current screen
-		if(back==null)
-		   back = (BufferedImage)(createImage(getWidth(),getHeight()));
+        
 
-		//create a graphics reference to the back ground image
-		//we will draw all changes on the background image
-		Graphics graphToBack = back.createGraphics();
+        
+        //add in collision detection to see if Bullets hit the Aliens and if Bullets hit the Ship
+        if (ship.getX() < 0) {
+            ship.setX(0);
+        }
+        if (ship.getX() > 800 - ship.getWidth()) {
+            ship.setX(800 - ship.getWidth());
+        }
+        if (ship.getY() < 0) {
+            ship.setY(0);
+        }
+        if (ship.getY() > 600 - ship.getHeight()) {
+            ship.setY(600 - ship.getHeight());
+        }
+        
+        /*for (Ammo a : bullet.getList()) {
+            if (a.getY() >= alienOne.getY() &&a.getY()<=alienOne.getY()+alienOne.getHeight()&&a.getX() >= alienOne.getX()&&a.getX()<= alienOne.getX()+alienOne.getWidth() ) {
+                alienOne.setHeight(0);
+                alienOne.setWidth(0);
+                a.setWidth(0);
+                a.setHeight(0);
+            }
+            if (a.getY() >= alienTwo.getY() && a.getY()<=alienTwo.getY()+alienTwo.getHeight()&& a.getX() >= alienTwo.getX()&&a.getX()<= alienTwo.getX()+alienTwo.getWidth()) {
+                alienTwo.setHeight(0);
+                alienTwo.setWidth(0);
+                a.setWidth(0);
+                a.setHeight(0);
+            }
+        }
+        */
+        horde.removeDeadOnes(bullet.getList());
+        twoDGraph.drawImage(back, null, 0, 0);
+    }
 
-		graphToBack.setColor(Color.BLUE);
-		graphToBack.drawString("StarFighter ", 25, 50 );
-		graphToBack.setColor(Color.BLACK);
-		graphToBack.fillRect(0,0,800,600);
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            keys[0] = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            keys[1] = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            keys[2] = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            keys[3] = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            keys[4] = true;
+        }
+        repaint();
+    }
 
-		if(keys[0] == true)
-		{
-                    ship.move("LEFT");
-		}
-                
-                if(keys[1] == true){
-                    ship.move("RIGHT");
-                }
-                
-                if(keys[2] == true) {
-                    ship.move("UP");
-                }
-                
-                if (keys[3] == true) {
-                    ship.move("DOWN");
-                }
-		//add code to move Ship, Alien, etc.
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            keys[0] = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            keys[1] = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            keys[2] = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            keys[3] = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            keys[4] = false;
+        }
+        repaint();
+    }
 
+    public void keyTyped(KeyEvent e) {
+        //no code needed here
+    }
 
-		//add in collision detection to see if Bullets hit the Aliens and if Bullets hit the Ship
-
-
-		twoDGraph.drawImage(back, null, 0, 0);
-	}
-
-
-	public void keyPressed(KeyEvent e)
-	{
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-		{
-			keys[0] = true;
-		}
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-		{
-			keys[1] = true;
-		}
-		if (e.getKeyCode() == KeyEvent.VK_UP)
-		{
-			keys[2] = true;
-		}
-		if (e.getKeyCode() == KeyEvent.VK_DOWN)
-		{
-			keys[3] = true;
-		}
-		if (e.getKeyCode() == KeyEvent.VK_SPACE)
-		{
-			keys[4] = true;
-		}
-		repaint();
-	}
-
-	public void keyReleased(KeyEvent e)
-	{
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-		{
-			keys[0] = false;
-		}
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-		{
-			keys[1] = false;
-		}
-		if (e.getKeyCode() == KeyEvent.VK_UP)
-		{
-			keys[2] = false;
-		}
-		if (e.getKeyCode() == KeyEvent.VK_DOWN)
-		{
-			keys[3] = false;
-		}
-		if (e.getKeyCode() == KeyEvent.VK_SPACE)
-		{
-			keys[4] = false;
-		}
-		repaint();
-	}
-
-	public void keyTyped(KeyEvent e)
-	{
-      //no code needed here
-	}
-
-   public void run()
-   {
-   	try
-   	{
-   		while(true)
-   		{
-   		   Thread.currentThread().sleep(5);
-            repaint();
-         }
-      }catch(Exception e)
-      {
-      }
-  	}
+    public void run() {
+        try {
+            while (true) {
+                Thread.currentThread().sleep(5);
+                repaint();
+            }
+        } catch (Exception e) {
+        }
+    }
 }
-
