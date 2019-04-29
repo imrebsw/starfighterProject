1e91ff56eaa934a42b6fe7034f0083d5362e9369
rebekah <33032837+imrebsw@users.noreply.github.com>
2019-04-29 07:30:39 -0700

    Update Bullets.java

--- src/starfighter/Bullets.java
+++ src/starfighter/Bullets.java
@@ -1,4 +1,5 @@
 package starfighter;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
@@ -15,7 +16,7 @@ public class Bullets {
     private List<Ammo> ammo;
 
     public Bullets() {
-        ammo = new ArrayList<Ammo>();
+        ammo = new ArrayList<>();
     }
 
     public void add(Ammo al) {
@@ -23,24 +24,33 @@ public class Bullets {
     }
 
     //post - draw each Ammo
-    public void drawEmAll(Graphics window) {
+    public void drawAll(Graphics window) {
         for (Ammo i : ammo) {
             i.draw(window);
         }
     }
 
-    public void moveEmAll() {
+    public void moveAll() {
         for (Ammo i : ammo) {
             i.move("UP");
+        }
+        for (int i = 0; i < ammo.size(); i++) {
+            if (ammo.get(i).getY() < 0) {
+                ammo.remove(i);
+                //ammo.get(i).setY(600);
+            }
         }
     }
 
     public void cleanEmUp() {
-        ammo.removeAll(ammo);
     }
 
     public List<Ammo> getList() {
         return ammo;
+    }
+    
+    public void remove(int i){
+        ammo.remove(i);
     }
 
     public String toString() {
