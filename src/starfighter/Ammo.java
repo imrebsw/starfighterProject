49ea07dc02f366062e58737ee79642f8510cc83a
rebekah <33032837+imrebsw@users.noreply.github.com>
2019-04-29 07:30:10 -0700

    Update Ammo.java

--- src/starfighter/Ammo.java
+++ src/starfighter/Ammo.java
@@ -3,55 +3,60 @@ package starfighter;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import javax.imageio.ImageIO;
 
-public class Ammo extends MovingThing
-{
-	private int speed;
+public class Ammo extends MovingThing {
 
-	public Ammo()
-	{
-		this(0,0,0);
-	}
+    private int speed;
 
-	public Ammo(int x, int y)
-	{
-		//add code
-	}
+    public Ammo() {
+        this(0, 0, 0);
+    }
 
-	public Ammo(int x, int y, int s)
-	{
-		//add code
-	}
+    public Ammo(int x, int y) {
+        this(x, y, 0);
+    }
 
-	public void setSpeed(int s)
-	{
-	   //add code
-	}
+    public Ammo(int x, int y, int s) {
+        super(x, y);
+        speed = s;
+    }
 
-	public int getSpeed()
-	{
-	   return 0;
-	}
+    public void setSpeed(int s) {
+        speed = s;
+    }
 
-	public void draw( Graphics window )
-	{
-		//add code to draw the ammo
-	}
-	
-	
-	public void move( String direction )
-	{
-		//add code to draw the ammo
-	}
+    public int getSpeed() {
+        return speed;
+    }
 
-	public String toString()
-	{
-		return "";
-	}
+    public void draw(Graphics window) {
+        window.setColor(Color.yellow);
+        window.fillRect(super.getX(), super.getY(), 10, 10);
+    }
+
+    public void move(String direction) {
+        switch (direction) {
+            case "UP":
+                setY(getY() - speed);
+                break;
+            case "DOWN":
+                setY(getY() + speed);
+                break;
+            case "RIGHT":
+                setX(getX() + speed);
+                break;
+            case "LEFT":
+                setX(getX() - speed);
+                break;
+        }
+    }
+
+    public String toString() {
+        return "";
+    }
 }
