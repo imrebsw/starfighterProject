97d7d6e5056d0303907fb43e58b5f88d9a5d4b23
rebekah <33032837+imrebsw@users.noreply.github.com>
2019-04-28 23:02:07 -0700

    Update Ship.java

--- src/starfighter/Ship.java
+++ src/starfighter/Ship.java
@@ -3,8 +3,6 @@ package starfighter;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
-
-import starfighter.MovingThing;
 import java.io.File;
 import java.net.URL;
 import java.awt.Color;
@@ -12,78 +10,66 @@ import java.awt.Graphics;
 import java.awt.Image;
 import javax.imageio.ImageIO;
 
-public class Ship extends MovingThing
-{
-	private int speed;
-	private Image image;
+public class Ship extends MovingThing {
 
-	public Ship()
-	{
-		this(10,10,10,10,10);
-	}
-        
-        public Ship(int x, int y)
-	{
-	  this(x, y, 50, 50);
-         //add code here
-	}
-        
-        public Ship(int x, int y, int s)
-	{
-	   this(x, y, 50, 50, s);
-           //add code here
-	}
+    private int speed;
+    private Image image;
 
-	public Ship(int x, int y, int w, int h, int s)
-	{
-		super(x, y, w, h);
-		speed=s;
-		try
-		{
-			URL url = getClass().getResource("/images/ship.jpg");
-			image = ImageIO.read(url);
-		}
-		catch(Exception e)
-		{
-			//feel free to do something here
-                    System.out.println(e);
-		}
-	}
+    public Ship() {
+        this(10, 10, 10, 10, 10);
+    }
 
+    public Ship(int x, int y) {
+        super(x, y);
+    }
 
-	public void setSpeed(int s)
-	{
-	   speed = s;   //add more code
-	}
+    public Ship(int x, int y, int s) {
+        //add code here
+        super(x, y);
+        speed = s;
 
-	public int getSpeed()
-	{
-	   return speed;
-	}
+    }
 
-	public void move(String direction)
-	{
-            if (direction.equals("LEFT")) {
-                setX(getX() - getSpeed());
-            }
-            else if (direction.equals("RIGHT")) {
-                setX(getX() + getSpeed());
-            }
-            if (direction.equals("UP")) {
-                setY(getY() - getSpeed());
-            }
-            else if (direction.equals("DOWN")) {
-                setY(getY() + getSpeed());
-            }
-	}
+    public Ship(int x, int y, int w, int h, int s) {
+        super(x, y, w, h);
+        speed = s;
+        try {
+            URL url = getClass().getResource("/images/ship.jpg");
+            image = ImageIO.read(url);
+        } catch (Exception e) {
+            //feel free to do something here
+        }
+    }
 
-	public void draw( Graphics window )
-	{
-   	window.drawImage(image,getX(),getY(),getWidth(),getHeight(),null);
-	}
+    public void setSpeed(int s) {
+        //add more code
+        speed = s;
+    }
 
-	public String toString()
-	{
-		return super.toString() + getSpeed();
-	}
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void move(String direction) {
+        if (direction.equals("DOWN")) {
+            super.setY(super.getY() + getSpeed());
+        }
+        if (direction.equals("UP")) {
+            super.setY(super.getY() - getSpeed());
+        }
+        if (direction.equals("LEFT")) {
+            super.setX(super.getX() - getSpeed());
+        }
+        if (direction.equals("RIGHT")) {
+            super.setX(super.getX() + getSpeed());
+        }
+    }
+
+    public void draw(Graphics window) {
+        window.drawImage(image, getX(), getY(), getWidth(), getHeight(), null);
+    }
+
+    public String toString() {
+        return super.toString() + getSpeed();
+    }
 }
